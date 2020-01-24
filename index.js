cf6a54fb166e0f3087d677353db589418f746d4e
obada <obadaquraan@yahoo.com>
2020-01-24 23:33:30 +0200

    lab-01-solved

--- index.js
+++ index.js
@@ -3,4 +3,11 @@
 
 const validator = require('./lib/validator.js');
 
-validator.isValid();
+
+
+console.log("validator.isValid('gg',validator.isNumber :", validator.isValid('gg',validator.isNumber));
+console.log("validator.isValid('gg',validator.isString :", validator.isValid('gg',validator.isString));
+console.log( "validator.isValid('gg',validator.isArray :", validator.isValid('gg',validator.isArray));
+console.log("validator.isValid('gg',validator.isBoolean :", validator.isValid('gg',validator.isBoolean));
+
+
